a839c24e237516daa37a9b4ab239bbb4eb9ee38c
huran199441 <353280368@qq.com>
2016-03-28 17:54:15 +0800

    add go border animate

--- js/event.js
+++ js/event.js
@@ -1,6 +1,6 @@
 
 window.onload=function(){
-    //��껮���ײ��µ�a��ǩ�������»���
+    //鼠标划过底部下的a标签，出现下划线
     var footTxt=$(".foot-txt a ");
     for(var i= 0;i<footTxt.length;i++){
         footTxt[i].onmouseover=function(){
@@ -10,4 +10,48 @@ window.onload=function(){
             this.style.textDecoration='none';
         }
     }
+
+    //鼠标滑过，go按钮出现边框
+    var lis=$(".detail-item li"),
+        go_border=$(".detail-item .go-border");
+    for(var i=0; i<lis.length;i++){
+        //为li标签创建索引
+        lis[i].index=i;
+        lis[i].onmouseover=function(){
+            //this变量引用当前划过的li
+            var _this=this;
+                go_border[_this.index].style.visibility='visible';
+        };
+        lis[i].onmouseout=function(){
+            var _this=this;
+            go_border[_this.index].style.visibility='hidden';
+        }
+    }
+
+
+
+    //轮播图
+    /*ar box=$("#slide-box"),
+        ul=$("#slide-img"),
+        li=ul.getElementsByTagName("li"),
+        img=ul.getElementsByTagName("img"),
+        prev_btn=$("#slide-btn1"),
+        next_btn=$("#slide-btn2"),
+        circle_btn=$("#slide-circle-btn");*/
+    var ul=document.getElementById("slide-img"),
+        li=ul.getElementsByTagName("li"),
+        len=0;
+
+    //点击前后按钮时
+
+    setInterval(function () {
+        for(var i=0; i<li.length; i++){
+            len++;
+            ul.style.left='-1350'*len+'px';
+        }
+    },3000);
+
+
+
+
 };
