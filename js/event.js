fdedc205976e2aa24db204d39963a0243b3b96d3
huran199441 <353280368@qq.com>
2016-04-04 19:36:50 +0800

    add slide-animate

--- js/event.js
+++ js/event.js
@@ -34,43 +34,78 @@
         $("#slide-circle-btn li").first().addClass("circle-btn-on");   //给第一个圆圈按钮添加类名
         var first_img = $("#slide-img li").first().clone();            //将第一张轮播图在最后克隆
         $("#slide-img").append(first_img);
-        var len = $("#slide-img li").length; //获取li标签的长度
+        var len = $("ul#slide-img li").length; //获取li标签的长度
 
         //鼠标滑动到当前圆圈按钮，相应轮播图出现
         $("#slide-circle-btn li").hover(function () {
             var index = $(this).index();     //获取当前元素的索引值
-            $("#slide-img").animate({left: -index * 1350}, 260);
+            l=index;                         //使当前索引值与轮播图相同
+            $("#slide-img").stop().animate({left: -index * 1350}, 260);
             $(this).addClass("circle-btn-on").siblings().removeClass("circle-btn-on");
 
+        });
 
+        //自动轮播
+        var time=setInterval(move_r,2000);
+
+        //鼠标滑到图片时，关定时器
+        $("ul#slide-img").hover(function () {
+            clearInterval(time);
+        },function(){
+            time=setInterval(move_r,2000);
+        });
+        $(".slide-btn").hover(function () {
+            clearInterval(time);
+        },function(){
+            time=setInterval(move_r,2000);
+        });
+        $("#slide-circle-btn").hover(function () {
+            clearInterval(time);
+        },function(){
+            time=setInterval(move_r,2000);
         });
 
         //当点击左按钮时
         $("#slide-btn1").click(function () {
+            move_l();
+        });
+
+        //点击向右按钮时
+        $("#slide-btn2").click(function () {
+            move_r();
+        });
+
+        //向左移动
+        function move_l(){
             l--;
             if (l == -1) {
                 $("#slide-img").css({left: -(len - 1) * 1350});
                 l = len - 2;
             }
-            $("#slide-img").animate({left: -l * 1350}, 1000);
+            $("ul#slide-img").stop().animate({left: -l * 1350}, 1000);
 
             //给当前的li标签添加类名，其他兄弟元素移除类名
-            $("#slide-circle-btn li").eq(l).addClass("circle-btn-on").siblings().removeClass("circle-btn-on");
+            $("ul#slide-circle-btn li").eq(l).addClass("circle-btn-on").siblings().removeClass("circle-btn-on");
 
-        });
-
-        //点击向右按钮时
-        $("#slide-btn2").click(function () {
+        }
+        //向右移动
+        function move_r(){
             l++;
             if (l == len) {
                 $("#slide-img").css({left: 0});
                 l = 1;
             }
-            $("#slide-img").animate({left: -l * 1350}, 800);
+            $("ul#slide-img").stop().animate({left: -l * 1350}, 800);
 
-            //当前图片下的圆圈按钮背景变为灰色
-            $("#slide-circle-btn li").eq(l).addClass("circle-btn-on").siblings().removeClass("circle-btn-on");
-        });
+            if (l == len - 1) {
+                //如果到最后一张图片，圆圈按钮的第一个显示为灰色
+                $("#slide-circle-btn li").eq(0).addClass("circle-btn-on").siblings().removeClass("circle-btn-on");
+            } else {
+                //当前图片下的圆圈按钮背景变为灰色
+                $("ul#slide-circle-btn li").eq(l).addClass("circle-btn-on").siblings().removeClass("circle-btn-on");
+            }
+
+        }
 
 
 
